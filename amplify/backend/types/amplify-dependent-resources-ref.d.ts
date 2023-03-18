54f8c0efb1014f746389b917def289f17ff0496b
Leamsi9 <ismaelv.dev@gmail.com>
2023-03-18 07:55:18 -0600

    schema improvements, amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,24 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "metadataAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "serverlessblo79c0d343": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AdminGroupRole": "string",
+      "authpoolGroupRole": "string"
+    }
+  }
+}
